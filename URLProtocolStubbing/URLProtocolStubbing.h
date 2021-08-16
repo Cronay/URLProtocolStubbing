25bbbf2ebbd94d60979f41c3ffcd4633d63abc6b
Cronay <inf@yannic-borgfeld.de>
2021-08-16 17:32:28 +0200

    Initial Commit

---
+++ URLProtocolStubbing/URLProtocolStubbing.h
@@ -0,0 +1,18 @@
+//
+//  URLProtocolStubbing.h
+//  URLProtocolStubbing
+//
+//  Created by Yannic Borgfeld on 16.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for URLProtocolStubbing.
+FOUNDATION_EXPORT double URLProtocolStubbingVersionNumber;
+
+//! Project version string for URLProtocolStubbing.
+FOUNDATION_EXPORT const unsigned char URLProtocolStubbingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLProtocolStubbing/PublicHeader.h>
+
+
